da5369c39394bfc29a8d44ad235b31e9b6412b2c
Matt-DevWeb <mv26@hotmail.fr>
2025-03-05 16:26:40 +0100

    Creating project SmartImplant

---
+++ src/main/java/com/isika/projet3/SmartImplant/SmartImplantApplication.java
@@ -0,0 +1,13 @@
+package com.isika.projet3.SmartImplant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartImplantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartImplantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/isika/projet3/SmartImplant/SmartImplantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isika.projet3.SmartImplant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartImplantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
